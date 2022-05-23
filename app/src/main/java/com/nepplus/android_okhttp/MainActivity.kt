da6528ee33edbfe2df6e356fb985a07b3fb3cb0c
sju01334 <sju01334@gmail.com>
2022-05-23 10:08:51 +0900

    xml 작업 및 데이터 바인딩 Main 구현

--- app/src/main/java/com/nepplus/android_okhttp/MainActivity.kt
+++ app/src/main/java/com/nepplus/android_okhttp/MainActivity.kt
@@ -2,10 +2,31 @@ package com.nepplus.android_okhttp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.nepplus.android_okhttp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var  binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setupEvents()
+        setValues()
+
+    }
+
+    fun setupEvents(){
+        binding.loginBtn.setOnClickListener {
+            val inputEmail = binding.emailEdt.text.toString()
+            val inputPW = binding.emailEdt.text.toString()
+        }
+
+    }
+
+    fun setValues(){
+
     }
 }
